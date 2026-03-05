6a920879a5c324e70d7eebc6aa61674052afef93
Aqsa Saher <aqsasaher5995@gmail.com>
2026-03-05 18:09:48 +0000

    Added error handling for invalid usernames

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ username = input("Enter GitHub username: ")
 url = f"https://api.github.com/users/{username}"
 response = requests.get(url)
 
-data = response.json()
-
-print("Name:", data["name"])
-print("Public repos:", data["public_repos"])
-print("Followers:", data["followers"])
+if response.status_code == 200:
+    data = response.json()
+    print("Name:", data["name"])
+    print("Public repos:", data["public_repos"])
+    print("Followers:", data["followers"])
+else:
+    print("User not found")
