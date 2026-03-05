f3175c14e9bb05fc103a32d8e1ac7eb403de0ad5
Aqsa Saher <aqsasaher5995@gmail.com>
2026-03-05 18:01:33 +0000

    Added GitHub analyzer code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("My first GitHub project")
+import requests
+
+username = input("Enter GitHub username: ")
+
+url = f"https://api.github.com/users/{username}"
+response = requests.get(url)
+
+data = response.json()
+
+print("Name:", data["name"])
+print("Public repos:", data["public_repos"])
+print("Followers:", data["followers"])
